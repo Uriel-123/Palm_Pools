56ba25273a60c22a3cd040dbf1e32b943209e3ad
Uriel <urielrodriguez2020@gmail.com>
2024-12-17 15:32:46 -0600

    updating file paths in images

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,18 +1,21 @@
-import React from 'react';
+const images = [
+  { src: '/images/jacket1.jpeg', alt: 'Jacket 1' },
+  { src: '/images/jacket2.jpeg', alt: 'Jacket 2' },
+  { src: '/images/jacket3.jpeg', alt: 'Jacket 3' },
+];
 
 const Gallery = () => {
-  const images = [
-    { src: '/images/jacket1.jpeg', alt: 'Jacket 1' },
-    { src: '/images/pool2.jpg', alt: 'Jacket 2' },
-    { src: '/images/pool3.jpg', alt: 'Jacket 3' },
-  ];
-
   return (
     <div>
       <h1>Gallery</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
         {images.map((image, index) => (
-          <img key={index} src={image.src} alt={image.alt} style={{ width: '200px', margin: '10px' }} />
+          <img
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            style={{ width: '200px', height: 'auto', margin: '10px' }}
+          />
         ))}
       </div>
     </div>
